58d3478821c57b68889f49f4736f89e74cf6f02e
ba1ake <blakegabb@gmail.com>
2025-02-27 10:37:34 +1300

    fixing git commit issue

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AirsoftTraderScraper
 A scraper designed to scrap through the New Zealand Airsoft trader website and Gather results, 
+
+
+scraper is desigend, or will be, to sraper the webpage and dump new listings into a text file, 
+
+
+I want to intergate in discord bot capablitlys
+
+
+will use text files in order to only post new listings up and later down the track will enable checking price changes
--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-import beautifulsoup4 as bs4
+import bs4
 import requests
+from bs4 import XMLParsedAsHTMLWarning
+import warnings
+warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning) #using HTML parser to parse XML so this removes error warning from bs4
 
 
 
+#rss feed url
+
+url = "https://airsofttrader.co.nz/feed/?post_type=ad_listing"
 
 
 
+#fetchs the rss feed from chosen URL and returns the content using HTML parser
+def fetch_rss(url):
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.text, 'html.parser') #need to change to XML
+    return soup
+
+def parse_rss_feed(content): #parses the rss feed and returns the title and description of each item provide no form of cleaning the data
+    items = content.find_all('item')
+    listings = []
+    for item in items:
+        title = item.find('title').text
+        description = bs4.BeautifulSoup(item.find('description').text, 'html.parser').get_text()
+        listings.append({'title': title, 'description': description})
+
+    return listings
 
 
-url = "https://airsofttrader.co.nz/nz-airsoft-sales/"
-response = requests.get(url)
-soup = bs4.BeautifulSoup(response.text, 'html.parser')
+rss = fetch_rss(url)
+sorted_listings = parse_rss_feed(rss)
 
-print(soup.prettify())
+print(sorted_listings[1])
