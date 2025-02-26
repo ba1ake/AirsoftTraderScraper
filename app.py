45a4cdf08d48df84ae52f3bedd0f33637128b9e3
ba1ake <blakegabb@gmail.com>
2025-02-27 10:00:11 +1300

    url added and soup made

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 import beautifulsoup4 as bs4
+import requests
+
+
+
+
+
+
+
+
+url = "https://airsofttrader.co.nz/nz-airsoft-sales/"
+response = requests.get(url)
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
