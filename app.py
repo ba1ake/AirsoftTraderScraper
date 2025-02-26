da366f15316c048e27c9c90d60d1f7b24f84476a
ba1ake <blakegabb@gmail.com>
2025-02-27 11:05:30 +1300

    removed HTML and useing XML praser

--- app.py
+++ app.py
@@ -1,33 +1,29 @@
 import bs4
 import requests
+from lxml import etree
 from bs4 import XMLParsedAsHTMLWarning
 import warnings
-warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning) #using HTML parser to parse XML so this removes error warning from bs4
+warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
 
-
-
-#rss feed url
-
+# RSS feed URL
 url = "https://airsofttrader.co.nz/feed/?post_type=ad_listing"
 
-
-
-#fetchs the rss feed from chosen URL and returns the content using HTML parser
+# Fetches the RSS feed from chosen URL and returns the content using XML parser
 def fetch_rss(url):
     response = requests.get(url)
-    soup = bs4.BeautifulSoup(response.text, 'html.parser') #need to change to XML
+    soup = bs4.BeautifulSoup(response.content, "xml") # Changed parser to XML
     return soup
 
-def parse_rss_feed(content): #parses the rss feed and returns the title and description of each item provide no form of cleaning the data
-    items = content.find_all('item')
-    listings = []
+# Parses the RSS feed and returns the title and description of each item into an array
+def parse_rss_feed(content):
+    items = content.find_all("item")
+    listings = [] #need to turn into multi layer array
     for item in items:
-        title = item.find('title').text
-        description = bs4.BeautifulSoup(item.find('description').text, 'html.parser').get_text()
-        listings.append({'title': title, 'description': description})
+        title = item.find("title").text
+        description = item.find("description").text
+        listings.append({"title": title, "description": description}) #appends as single layer array
 
     return listings
-
 
 rss = fetch_rss(url)
 sorted_listings = parse_rss_feed(rss)
