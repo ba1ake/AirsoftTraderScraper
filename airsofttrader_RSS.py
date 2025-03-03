7894fc1c026e13435dfa7dc077adf576853a6d27
ba1ake <blakegabb@gmail.com>
2025-03-03 19:50:18 +1300

    added spacing to document

--- airsofttrader_RSS.py
+++ airsofttrader_RSS.py
@@ -13,25 +13,29 @@ def write_to_file(content, filename):
 
 def append_to_file(content, filename):
     with open(filename, "a", encoding="utf-8") as file:
-        file.write(str(content))
+        file.write(str(content)+ '["void","void"]')
     return True
 
 def read_from_file(filename):
     with open(filename, "r", encoding="utf-8") as file:
         file_contents_array = []
         file_contents = file.readlines()
+
         for line in file_contents:
             parts = line.split(",")
             title, description = parts[0], parts[1]
             file_contents_array.append([title, description])
+
     if len(file_contents_array) == 0:
         return False
+
     return file_contents_array
 
 def replace_special_tag(text):
-    special_tags = ["&#8217;", "&amp;", "\n", "\t", "\r", "<p>", "</p>", "<br>", "</br>", "<br />","\x84"]
-    replacements = ["'", "&", "", "", "", "", "", "", "", "", "", ""]
+    special_tags = ["&#8217;", "&amp;", "\n", "\t", "\r", "<p>", "</p>", "<br>", "</br>", "<br />""<a/>","\x84"]
+    replacements = ["'", "&", "", "", "", "", "", "", "", "", "", "",""]
     cleaned_text = text
+
     for tag, replacement in zip(special_tags, replacements):
         cleaned_text = re.sub(tag, replacement, cleaned_text)
     return cleaned_text
@@ -41,14 +45,18 @@ def fetch_rss(url):
         feed = feedparser.parse(url)
         if feed.bozo:
             raise Exception(feed.bozo_exception)
+
         return feed
+
     except Exception as err:
         print(f"An error occurred: {err}")
         return None
 
 def parse_rss_feed(feed):
+
     listings = []
     links = []
+
     for entry in feed.entries:
         link = entry.link
         title = entry.title
@@ -58,20 +66,24 @@ def parse_rss_feed(feed):
     return listings, links
 
 def generate_listings_all(url):
+
     rss = fetch_rss(url)
     listing_urls = []
     sorted_listings, listing_urls = parse_rss_feed(rss)
     complete_listings = []
     plain_text = ""
+
     for item in sorted_listings:
         title = item[0]
         description = item[1]
         complete_listings.append([title, description])
         plain_text += str(complete_listings[len(complete_listings)-1]) + "\n"
+
     write_to_file(plain_text, "newlistings.txt")
     return complete_listings, listing_urls
 
 def check_new_listings(current_listing_item, old_listings):
+    
     for old_listing_item in old_listings:
         old_listing_item_str = str(old_listing_item[0])
         old_listing_item_str = old_listing_item_str[2:-1]
